c0754e389e770e330d5db2308a4506932dc5029f
Gábor László <gopher.hu@gmail.com>
2017-04-24 01:25:05 +0200

    Autoloader added

---
+++ autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+function __autoload($className) {
+	static $fileIterator;
+	if (is_null($fileIterator)) {
+		$directory = new RecursiveDirectoryIterator(__DIR__.'/components', RecursiveDirectoryIterator::SKIP_DOTS);
+		$fileIterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::LEAVES_ONLY);
+	}
+	$filename = $className.'.class.php';
+	foreach ($fileIterator as $file) {
+		if (strtolower($file->getFilename()) === strtolower($filename)) {
+			if ($file->isReadable()) {
+				include_once $file->getPathname();
+			}
+			break;
+		}
+	}
+}
--- index.php
+++ index.php
@@ -1,44 +1,8 @@
 <?php
 
-include "components/core/config.class.php";
-include "components/core/router.class.php";
-include "components/core/request.class.php";
-include "components/core/response.class.php";
-include "components/core/user.class.php";
-include "components/core/view.class.php";
-include "components/core/form.class.php";
-include "components/core/validator.class.php";
-include "components/core/validators/notEmptyValidator.class.php";
-include "components/core/input.class.php";
-include "components/core/inputs/textInput.class.php";
-include "components/core/inputs/passwordInput.class.php";
-include "components/core/inputs/hiddenInput.class.php";
-include "components/core/db.class.php";
-include "components/core/dbException.class.php";
-include "components/core/dbResult.class.php";
-include "components/core/column.class.php";
-include "components/core/columns/integerColumn.class.php";
-include "components/core/columns/stringColumn.class.php";
-include "components/core/columns/booleanColumn.class.php";
-include "components/core/table.class.php";
-include "components/core/record.class.php";
-include "components/core/controller.class.php";
-include "components/core/webApplication.class.php";
-
-include "components/welcome/WelcomeController.class.php";
-
-include "components/user/userTable.class.php";
-include "components/user/userModel.class.php";
-include "components/user/loginForm.class.php";
-include "components/user/loginController.class.php";
-include "components/user/logoutController.class.php";
-include "components/user/profileController.class.php";
-
+include_once "autoload.php";
 
 $config = new Config();
-$request = new Request();
-$response = new Response();
-
 $config->set('db.default.host', 'localhost');
 $config->set('db.default.name', 'a');
 $config->set('db.default.user', 'root');
@@ -51,7 +15,9 @@ $config->set('router.default.controller', 'WelcomeController');
 $config->set('router.default.method', 'index');
 $config->set('view.template.extension', 'phtml');
 $config->set('user.salt', '!#user%$salt');
- 
+
+$request = new Request();
+$response = new Response();
 $db = new DB($config);
 $view = new View($config);
 $user = new User($config, $request);
